6201365a0e9fa6e82a1ddb54496b5988605b272b
joiingiProg <joiingi@gmail.com>
2013-11-18 14:33:31 +0100

    Graphics Tutorial

---
+++ js/signup.js
@@ -0,0 +1,98 @@
+var nPage = 1;
+$(document).ready(function(){
+	
+//    Login in Button
+    $('#btn-login').click(function(){
+		$.mobile.changePage('#phosom-index');
+	});
+
+//    SignUP in Button		    
+	$('#btn-signup').click(function(){	
+		$.mobile.changePage('#tutorial1');
+	});
+        
+    
+    //$('#btn-next').click(function(){
+    //    nPage = nPage + 1;
+    //   $.mobile.changePage('#tutorial' + nPage); 
+    //});
+    $('#btn-next').click(function(){
+           $.mobile.changePage('#tutorial2'); 
+    });
+    $('#btn-next2').click(function(){
+           $.mobile.changePage('#tutorial3'); 
+    });
+    $('#btn-next3').click(function(){
+           $.mobile.changePage('#tutorial4'); 
+    });
+    $('#btn-next4').click(function(){
+           $.mobile.changePage('#tutorial5'); 
+    });
+    $('#btn-next5').click(function(){
+           $.mobile.changePage('#tutorial6'); 
+    });
+    $('#btn-next6').click(function(){
+           $.mobile.changePage('#tutorial7'); 
+    });
+    $('#btn-next7').click(function(){
+           $.mobile.changePage('#tutorial8'); 
+    });
+    $('#btn-next8').click(function(){
+           $.mobile.changePage('#tutorial9'); 
+    });
+    $('#btn-next9').click(function(){
+           $.mobile.changePage('#tutorial10'); 
+    });
+    $('#btn-next10').click(function(){
+           $.mobile.changePage('#tutorial11'); 
+    });
+    $('#btn-next11').click(function(){
+           $.mobile.changePage('#tutorial12'); 
+    });
+    $('#btn-next12').click(function(){
+           $.mobile.changePage('#tutorial13'); 
+    });
+    $('#btn-next13').click(function(){
+           $.mobile.changePage('#tutorial14'); 
+    });
+    $('#btn-next14').click(function(){
+           $.mobile.changePage('#tutorial15'); 
+    });
+    $('#btn-next15').click(function(){
+           $.mobile.changePage('#tutorial16'); 
+    });
+    $('#btn-next16').click(function(){
+           $.mobile.changePage('#tutorial17'); 
+    });
+    $('#btn-next17').click(function(){
+           $.mobile.changePage('#tutorial18'); 
+    });
+    $('#btn-next18').click(function(){
+           $.mobile.changePage('#tutorial19'); 
+    });
+    $('#btn-next19').click(function(){
+           $.mobile.changePage('#tutorial20'); 
+    });
+    $('#btn-next20').click(function(){
+           $.mobile.changePage('#tutorial21'); 
+    });
+    $('#btn-next21').click(function(){
+           $.mobile.changePage('#tutorial22'); 
+    });
+    $('#btn-next22').click(function(){
+           $.mobile.changePage('#tutorial23'); 
+    });
+    $('#btn-next23').click(function(){
+           $.mobile.changePage('#tutorial24'); 
+    });
+    $('#btn-next24').click(function(){
+           $.mobile.changePage('#tutorial25'); 
+    });
+    $('#btn-next25').click(function(){
+           $.mobile.changePage('#tutorial26'); 
+    });
+    $('#btn-next26').click(function(){
+           $.mobile.changePage('#phosom-index'); 
+    });
+   
+});
